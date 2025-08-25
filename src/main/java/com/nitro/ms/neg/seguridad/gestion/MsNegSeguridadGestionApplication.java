be1a4453b5b9583d2d4a1c1eed528af0eea4d9d4
Nicolas Santillan <adrian.santillan@ajegroup.com>
2025-08-25 06:56:02 -0500

    chore: initial project setup

---
+++ src/main/java/com/nitro/ms/neg/seguridad/gestion/MsNegSeguridadGestionApplication.java
@@ -0,0 +1,13 @@
+package com.nitro.ms.neg.seguridad.gestion;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsNegSeguridadGestionApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsNegSeguridadGestionApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/nitro/ms/neg/seguridad/gestion/MsNegSeguridadGestionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nitro.ms.neg.seguridad.gestion;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsNegSeguridadGestionApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
